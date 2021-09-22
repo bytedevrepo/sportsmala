5865ba1bf58f96f1f28389d232d99b696407dbb6
Bishal Khatri <bishal.khatri343@gmail.com>
2021-09-22 16:10:51 +0545

    footer category widget updated

--- resources/views/theme-soccer/widgets/footer/categories.blade.php
+++ resources/views/theme-soccer/widgets/footer/categories.blade.php
@@ -4,15 +4,25 @@
         <div class="widget widget--footer widget-popular-posts">
             <h4 class="widget__title">{{ data_get($detail, 'title') }}</h4>
             <div class="widget__content">
-                <ol class="list">
+                <ul class="posts posts--simple-list">
                     @foreach($content as $item )
-                        @if ($loop->index > 6)
+                        @if ($loop->index > 10)
                             @break
                         @endif
-                        <li><a href="{{ url('category',$item->slug) }}">{{ $item->category_name }} <span>({{ $item->post_count }})</span></a></li>
+                            <li class="posts__item posts__item--category-1">
+                                <div class="posts__inner">
+                                    <h6 class="posts__title posts__title--color-hover">
+                                        <a href="{{ url('category',$item->slug) }}">
+                                            {{ $item->category_name }} <span class="float-right">({{ $item->post_count }})</span>
+                                        </a>
+                                    </h6>
+                                </div>
+                            </li>
                     @endforeach
-                </ol>
+                </ul>
             </div>
         </div>
+        <!-- Widget: Popular Posts / End -->
     </div>
 </div>
+
