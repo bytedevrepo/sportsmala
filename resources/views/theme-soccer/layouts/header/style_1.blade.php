6624c9ad4b4e9e388bfd4f7b64a73d01bdc67264
Bishal Khatri <bishal.khatri343@gmail.com>
2022-02-02 11:02:28 +0545

    css patch from arjun sir included

--- resources/views/theme-soccer/layouts/header/style_1.blade.php
+++ resources/views/theme-soccer/layouts/header/style_1.blade.php
@@ -74,9 +74,9 @@
                 @include('tournament::score-card')
             </div>
         </div>
-    @endif
+@endif
 
-    <!-- Header Secondary / End -->
+<!-- Header Secondary / End -->
     <!-- Header Primary -->
     <div class="header__primary">
         <div class="container">
@@ -90,111 +90,112 @@
                 <!-- Header Logo / End -->
                 <!-- Main Navigation -->
                 <nav class="main-nav clearfix">
-                    <ul class="main-nav__list">
-                        @foreach($primaryMenu as $mainMenu)
-                            @if($mainMenu->is_mega_menu == 'no')
-                                <li class="">
-                                    <a href="{{menuUrl($mainMenu)}}" target="{{$mainMenu->new_teb == 1? '_blank':''}}">{{$mainMenu->label == 'gallery'? __('gallery'):$mainMenu->label}}</a>
-                                    @if(!blank($mainMenu->children))
-                                        <ul class="main-nav__sub">
-                                            @foreach($mainMenu->children as $child)
-                                                <li class="">
-                                                    <a href="{{menuUrl($child)}}" target="{{$child->new_teb == 1? '_blank':''}}">{{$child->label == 'gallery'? __('gallery'):$child->label}}</a>
-                                                    @if(!blank($child->children))
-                                                        <ul class="main-nav__sub-2">
-                                                            @foreach($child->children as $subChild)
-                                                                <li>
-                                                                    <a href="{{menuUrl($subChild)}}" target="{{$subChild->new_teb == 1? '_blank':''}}">{{$subChild->label == 'gallery'? __('gallery'):$subChild->label}}</a>
-                                                                </li>
-                                                            @endforeach
-                                                        </ul>
-                                                    @endif
-                                                </li>
-                                            @endforeach
-                                        </ul>
-                                    @endif
-                                </li>
-                            @endif
-                            @if($mainMenu->is_mega_menu == 'category')
-                                <li class="">
-                                    <a href="{{menuUrl($mainMenu)}}" target="{{$mainMenu->new_teb == 1 ? '_blank':''}}">{{$mainMenu->label == 'gallery'? __('gallery'):$mainMenu->label}}</a>
-                                    <div class="main-nav__megamenu clearfix">
-                                        @foreach($mainMenu->children as $child)
-                                            <ul class="col-lg-4 col-md-4 col-12 main-nav__ul">
-                                                <li class="main-nav__title">{{$child->label == 'gallery'? __('gallery'):$child->label}}</li>
-                                                @foreach($child->children as $subChild)
-                                                    <li>
-                                                        <a href="{{menuUrl($subChild)}}" target="{{$subChild->new_teb == 1? '_blank':''}}">{{$subChild->label == 'gallery'? __('gallery'):$subChild->label}}</a>
+                    <div class="skew-bg">
+                        <ul class="main-nav__list">
+                            @foreach($primaryMenu as $mainMenu)
+                                @if($mainMenu->is_mega_menu == 'no')
+                                    <li class="">
+                                        <a href="{{menuUrl($mainMenu)}}" target="{{$mainMenu->new_teb == 1? '_blank':''}}">{{$mainMenu->label == 'gallery'? __('gallery'):$mainMenu->label}}</a>
+                                        @if(!blank($mainMenu->children))
+                                            <ul class="main-nav__sub">
+                                                @foreach($mainMenu->children as $child)
+                                                    <li class="">
+                                                        <a href="{{menuUrl($child)}}" target="{{$child->new_teb == 1? '_blank':''}}">{{$child->label == 'gallery'? __('gallery'):$child->label}}</a>
+                                                        @if(!blank($child->children))
+                                                            <ul class="main-nav__sub-2">
+                                                                @foreach($child->children as $subChild)
+                                                                    <li>
+                                                                        <a href="{{menuUrl($subChild)}}" target="{{$subChild->new_teb == 1? '_blank':''}}">{{$subChild->label == 'gallery'? __('gallery'):$subChild->label}}</a>
+                                                                    </li>
+                                                                @endforeach
+                                                            </ul>
+                                                        @endif
                                                     </li>
                                                 @endforeach
                                             </ul>
-                                        @endforeach
-                                    </div>
-                                </li>
-                            @endif
-                            @if($mainMenu->is_mega_menu == 'tab')
-                                <li class="">
-                                    <a href="{{menuUrl($mainMenu)}}">{{$mainMenu->label == 'gallery'? __('gallery'):$mainMenu->label}}</a>
-                                    <div class="main-nav__megamenu clearfix">
-                                        <ul class="col-lg-3 col-md-4 col-12 main-nav__ul">
-                                            <li class="main-nav__title">Features</li>
-                                            <li><a href="_soccer_features-shortcodes.html">Shortcodes</a></li>
-                                            <li><a href="_soccer_features-typography.html">Typography</a></li>
-                                            <li><a href="_soccer_features-widgets-blog.html">Widgets - Blog</a></li>
-                                            <li><a href="_soccer_features-widgets-shop.html">Widgets - Shop</a></li>
-                                            <li><a href="_soccer_features-widgets-sports.html">Widgets - Sports</a></li>
-                                            <li><a href="_soccer_features-404.html">404 Error Page</a></li>
-                                            <li><a href="_soccer_features-search-results.html">Search Results</a></li>
-                                            <li><a href="_soccer_page-contacts.html">Contact Us</a></li>
-                                        </ul>
-
-
-                                        <div class="col-lg-9 col-md-8 col-12">
-                                            <ul class="posts posts--simple-list">
-                                                <li class="posts__item posts__item--category-1">
-                                                    <figure class="posts__thumb">
-                                                        <a href="#">
-                                                            <img src="/site/theme-soccer/assets/images/samples/post-img3-xs.jpg" alt="">
-                                                        </a>
-                                                    </figure>
-                                                    <div class="posts__inner">
-                                                        <div class="posts__cat"><span class="label posts__cat-label">The Team</span></div>
-                                                        <h6 class="posts__title"><a href="#">The new eco friendly stadium won a Leafy Award in 2016</a></h6>
-                                                        <time datetime="2016-08-21" class="posts__date">August 21st, 2016</time>
-                                                    </div>
-                                                </li>
-                                                <li class="posts__item posts__item--category-2">
-                                                    <figure class="posts__thumb">
-                                                        <a href="#">
-                                                            <img src="/site/theme-soccer/assets/images/samples/post-img1-xs.jpg" alt="">
-                                                        </a>
-                                                    </figure>
-                                                    <div class="posts__inner">
-                                                        <div class="posts__cat"><span class="label posts__cat-label">Injuries</span></div>
-                                                        <h6 class="posts__title"><a href="#">Mark Johnson has a Tibia Fracture and is gonna be out</a></h6>
-                                                        <time datetime="2016-08-23" class="posts__date">August 23rd, 2016</time>
-                                                    </div>
-                                                </li>
-                                                <li class="posts__item posts__item--category-1">
-                                                    <figure class="posts__thumb">
-                                                        <a href="#">
-                                                            <img src="/site/theme-soccer/assets/images/samples/post-img4-xs.jpg" alt="">
-                                                        </a>
-                                                    </figure>
-                                                    <div class="posts__inner">
-                                                        <div class="posts__cat"><span class="label posts__cat-label">The Team</span></div>
-                                                        <h6 class="posts__title"><a href="#">The team is starting a new power breakfast regimen</a></h6>
-                                                        <time datetime="2016-08-21" class="posts__date">August 21st, 2016</time>
-                                                    </div>
-                                                </li>
-                                            </ul>
+                                        @endif
+                                    </li>
+                                @endif
+                                @if($mainMenu->is_mega_menu == 'category')
+                                    <li class="">
+                                        <a href="{{menuUrl($mainMenu)}}" target="{{$mainMenu->new_teb == 1 ? '_blank':''}}">{{$mainMenu->label == 'gallery'? __('gallery'):$mainMenu->label}}</a>
+                                        <div class="main-nav__megamenu clearfix">
+                                            @foreach($mainMenu->children as $child)
+                                                <ul class="col-lg-4 col-md-4 col-12 main-nav__ul">
+                                                    <li class="main-nav__title">{{$child->label == 'gallery'? __('gallery'):$child->label}}</li>
+                                                    @foreach($child->children as $subChild)
+                                                        <li>
+                                                            <a href="{{menuUrl($subChild)}}" target="{{$subChild->new_teb == 1? '_blank':''}}">{{$subChild->label == 'gallery'? __('gallery'):$subChild->label}}</a>
+                                                        </li>
+                                                    @endforeach
+                                                </ul>
+                                            @endforeach
                                         </div>
-                                    </div>
-                                </li>
-                            @endif
-                        @endforeach
-                    </ul>
+                                    </li>
+                                @endif
+                                @if($mainMenu->is_mega_menu == 'tab')
+                                    <li class="">
+                                        <a href="{{menuUrl($mainMenu)}}">{{$mainMenu->label == 'gallery'? __('gallery'):$mainMenu->label}}</a>
+                                        <div class="main-nav__megamenu clearfix">
+                                            <ul class="col-lg-3 col-md-4 col-12 main-nav__ul">
+                                                <li class="main-nav__title">Features</li>
+                                                <li><a href="_soccer_features-shortcodes.html">Shortcodes</a></li>
+                                                <li><a href="_soccer_features-typography.html">Typography</a></li>
+                                                <li><a href="_soccer_features-widgets-blog.html">Widgets - Blog</a></li>
+                                                <li><a href="_soccer_features-widgets-shop.html">Widgets - Shop</a></li>
+                                                <li><a href="_soccer_features-widgets-sports.html">Widgets - Sports</a></li>
+                                                <li><a href="_soccer_features-404.html">404 Error Page</a></li>
+                                                <li><a href="_soccer_features-search-results.html">Search Results</a></li>
+                                                <li><a href="_soccer_page-contacts.html">Contact Us</a></li>
+                                            </ul>
 
+
+                                            <div class="col-lg-9 col-md-8 col-12">
+                                                <ul class="posts posts--simple-list">
+                                                    <li class="posts__item posts__item--category-1">
+                                                        <figure class="posts__thumb">
+                                                            <a href="#">
+                                                                <img src="/site/theme-soccer/assets/images/samples/post-img3-xs.jpg" alt="">
+                                                            </a>
+                                                        </figure>
+                                                        <div class="posts__inner">
+                                                            <div class="posts__cat"><span class="label posts__cat-label">The Team</span></div>
+                                                            <h6 class="posts__title"><a href="#">The new eco friendly stadium won a Leafy Award in 2016</a></h6>
+                                                            <time datetime="2016-08-21" class="posts__date">August 21st, 2016</time>
+                                                        </div>
+                                                    </li>
+                                                    <li class="posts__item posts__item--category-2">
+                                                        <figure class="posts__thumb">
+                                                            <a href="#">
+                                                                <img src="/site/theme-soccer/assets/images/samples/post-img1-xs.jpg" alt="">
+                                                            </a>
+                                                        </figure>
+                                                        <div class="posts__inner">
+                                                            <div class="posts__cat"><span class="label posts__cat-label">Injuries</span></div>
+                                                            <h6 class="posts__title"><a href="#">Mark Johnson has a Tibia Fracture and is gonna be out</a></h6>
+                                                            <time datetime="2016-08-23" class="posts__date">August 23rd, 2016</time>
+                                                        </div>
+                                                    </li>
+                                                    <li class="posts__item posts__item--category-1">
+                                                        <figure class="posts__thumb">
+                                                            <a href="#">
+                                                                <img src="/site/theme-soccer/assets/images/samples/post-img4-xs.jpg" alt="">
+                                                            </a>
+                                                        </figure>
+                                                        <div class="posts__inner">
+                                                            <div class="posts__cat"><span class="label posts__cat-label">The Team</span></div>
+                                                            <h6 class="posts__title"><a href="#">The team is starting a new power breakfast regimen</a></h6>
+                                                            <time datetime="2016-08-21" class="posts__date">August 21st, 2016</time>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </li>
+                                @endif
+                            @endforeach
+                        </ul>
+                    </div>
                 {{--<a href="#" class="pushy-panel__toggle"><span class="pushy-panel__line"></span> </a>--}}
                 <!-- Pushy Panel Toggle / Eng -->
                     <div class="slan-comp"></div>
